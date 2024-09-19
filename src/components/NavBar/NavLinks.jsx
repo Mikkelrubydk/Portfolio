9d343dfc8a16c9531c57e84281459c925d45af22
MikkelRuby <mikkelruby1@hotmail.com>
2024-09-20 00:45:19 +0200

    Update NavLinks.jsx

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,21 +1,37 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const NavLinks = () => {
+  const location = useLocation();
+
+  // Når URL'en ændres, scroll til den sektion der svarer til hash
+  useEffect(() => {
+    // Hvis der er en hash i URL'en
+    if (location.hash) {
+      // Få elementet med id svarende til hash
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        // Scroll til det element
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
   return (
     <nav className="NavLinks">
       <ul>
         <li>
-          <Link to="/">Hjem</Link>
+          <Link to="/#forside">Hjem</Link>
         </li>
         <li>
-          <Link to="/">Kompetencer</Link>
+          <Link to="/#kompetencer">Kompetencer</Link>
         </li>
         <li>
-          <Link to="/">Om mig</Link>
+          <Link to="/#ommig">Om mig</Link>
         </li>
         <li>
-          <Link to="/">Projekter</Link>
+          <Link to="/#projekter">Projekter</Link>
         </li>
 
         <figure className="socials">
